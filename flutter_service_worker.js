3f96dc3805b85962d219701e06764b02c18490eb
simple <383559698@qq.com>
2022-06-28 15:42:01 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f3ddd461dc69ad51a1cfde1e44df5349",
-"index.html": "ecda232bcf55e4f968b14055de2e8bd2",
-"/": "ecda232bcf55e4f968b14055de2e8bd2",
-"main.dart.js": "6153708cf6a372865dee8ea812f8a072",
+"index.html": "b1bb1e12fea31627d667559d7f73d704",
+"/": "b1bb1e12fea31627d667559d7f73d704",
+"main.dart.js": "47192cb2a564b266ff6508c494999f05",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
